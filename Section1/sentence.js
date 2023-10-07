5d537b86867e5374b0aac8c6ee3e5709b94d29e0
Fatemeh Saei <fsaee1380@gmail.com>
2023-10-08 00:11:25 +0330

    new update

--- Section1/sentence.js
+++ Section1/sentence.js
@@ -2,6 +2,15 @@ const sentence = "salam man fatemeh hastam - salam fatemeh";
 const words = sentence.split(" ");
 
 const wordCount = {};
+/*
+words.forEach(word =>{
+    if(wordCount[word]){
+        wordCount[word] += 1;
+    }else{
+        wordCount[word] = 1;
+    }
+})
+*/
 
 for(let word of words){
     if(!word){
---
+++ Section16/closure.js
@@ -0,0 +1,13 @@
+function outer() {
+    let outerVar = 10;
+    function inner() {
+        outerVar += 1;
+        console.log(outerVar); // inner function can access outerVar
+    }
+    return inner;
+}
+
+const result = outer();
+result();
+result();
+result();
